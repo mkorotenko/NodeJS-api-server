78d5016fa783ccecd51cdf22c7c0868e4097a5b6
Maxim Korotenko <m.v.korotenko@gmail.com>
2018-04-05 20:40:28 +0300

    Last changes

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-var express = require('express'),
+const express = require('express'),
+  bodyParser = require('body-parser'),
   app = express(),
   port = process.env.PORT || 3000;
 
-var routes = require('./routes'); //importing route
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+  
+const routes = require('./routes'); //importing route
 routes(app); //register the route
 
 app.listen(port);
